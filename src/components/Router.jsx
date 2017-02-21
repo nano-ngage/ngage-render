9a07856f27e465ad8566bfff38c76093aa844e4a
Saivickna Raveendran <saivickna.r@gmail.com>
2017-02-21 11:57:14 -0800

    (feat) Routes to home if the user is not in a session

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -7,6 +7,7 @@ import App from './App';
 import Home from '../containers/Home';
 import Presenter from '../containers/Presenter';
 import Viewer from '../containers/Viewer';
+import Presentation from '../containers/Presentation';
 import NoMatch from './NoMatch';
 import store from '../store/index';
 
@@ -18,13 +19,20 @@ window.browserHistory = browserHistory;
 // var socket = io.connect('http://localhost:5500');
 // window.socket = socket;
 
+function sessionOnly(store) {
+  if (!store.getState().session) {
+    browserHistory.replace('/');
+  }
+}
+
 export default () => (
     <Provider store={store}>
       <Router history={ browserHistory }>
         <Route component={ App }>
           <IndexRoute component={ Home }/>
-          <Route path="/presenter" component={ Presenter }/>
-          <Route path="/viewer" component={ Viewer }/>
+          <Route path="/presenter" component={ Presenter } onEnter = {() => {sessionOnly(store)}}/>
+          <Route path="/viewer" component={ Viewer } onEnter = {() => {sessionOnly(store)}}/>
+          <Route path="/presentation" component={ Presentation }/>
           <Route path="*" component={ NoMatch }/>
         </Route>
       </Router>
