7a239c47d7320ef072b3b0101e44563bb0b9253c
pshen <patrick.lc.shen@gmail.com>
2017-02-24 20:58:10 -0800

    (feat) remove input clearing

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -8,7 +8,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'inferno-redux';
 
 function roomVal(instance, e) {
-  instance.setState({room: e.target.value});  
+  instance.setState({room: e.target.value});
 }
 
 class Home extends Component {
@@ -27,22 +27,22 @@ class Home extends Component {
 
   render() {
     //const auth = new AuthService('qjGwtKBFdcc0chj52rGul3p3nEa0LW3J', 'saivickna.auth0.com', this.props);
-    
+
     return (
       <div>
         <div>
-          {this.props.user ? (<div className='login'><h1 className="welcome">Welcome to nGage, {this.props.user.firstName + ' ' + this.props.user.lastName}</h1></div>) 
+          {this.props.user ? (<div className='login'><h1 className="welcome">Welcome to nGage, {this.props.user.firstName + ' ' + this.props.user.lastName}</h1></div>)
           : (<div className='login'><h1 className="welcome">Welcome to nGage</h1></div>)}
         </div>
           {this.props.invalidRoom === 'invalid' ? (<div className='login'>
-          <p className="normal">Invalid Room</p></div>) : ('')}
+          <p className="normal animated fadeInDown invalid">Invalid Room</p></div>) : ('')}
         <div className='room'>
-          <input className='codeInput' value={this.state.room} onInput={linkEvent(this, roomVal)} type="text" placeholder="Room Code" 
-            onKeyPress={(e) => {if (e.which === 13) {this.props.setSession({socket: this.state.room, userID: this.props.user ? this.props.user.userID : -1})}}}></input>
+          <input className='codeInput' value={this.state.room} onInput={linkEvent(this, roomVal)} type="text" placeholder="Room Code"
+            onKeyPress={(e) => {if (e.which === 13) {this.props.setSession({socket: this.state.room, userID: this.props.user ? this.props.user.userID : -1})};}}></input>
           <button className='button' onClick={() => {this.props.setSession({socket: this.state.room, userID: this.props.user ? this.props.user.userID : -1})}}>Enter</button>
         </div>
         <div>
-          {this.props.user ? (<div className='login'><button onClick={this.state.auth.logout.bind(this)}  className='button'>Logout</button></div>) 
+          {this.props.user ? (<div className='login'><button onClick={this.state.auth.logout.bind(this)}  className='button'>Logout</button></div>)
           : (<div className='login'><button onClick={this.state.auth.login.bind(this)}  className='button'>Login</button></div>)}
         </div>
       </div>
@@ -57,7 +57,7 @@ const mapStateToProps = state => {
     room: state.room,
     invalidRoom: state.invalidRoom
   };
-} 
+}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators( {setPresenter, setViewer, setUser, setSession}, dispatch);
