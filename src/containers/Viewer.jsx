16fb3db44d8601cdb6704f92c4ed9153fc62fc56
pshen <patrick.lc.shen@gmail.com>
2017-02-25 12:02:12 -0800

    (feat) format columns

--- src/containers/Viewer.jsx
+++ src/containers/Viewer.jsx
@@ -14,17 +14,21 @@ class Viewer extends Component {
   }
   render() {
     var i = 0;
-    var colors = ["#ffd380", "#71b2fb", "#b195c6", "#ffb1b1"];
+    var colors = ["#ffb1b1", "#ffd380", "#71b2fb", " #b195c6"];
     return (
       <div className="content">
         {this.props.answers ? (
           <div>
-            <div className="center"><p className="normal"><h1>{this.props.answers.question.question}</h1> <br /><br/></p>
+            <div className="normal center"><h1>{this.props.answers.question.question}</h1>
             </div>
-            <div className="center">
+            <div className="answer-container">
             {this.props.answers.answers.map(answer => {
-              i++;
-              return <button className="answerButtons" style={"background-color: "+ colors[i]} onClick={() => {this.props.submitAnswer({question: this.props.answers.question, answer:answer})}} key={answer.answerID}>{answer.answer}</button>})}
+              if (i < 4) {
+                i++;
+              } else {
+                i = 0;
+              }
+              return <button className="answerButtons" style={"background-color: "+ colors[i - 1]} onClick={() => {this.props.submitAnswer({question: this.props.answers.question, answer:answer})}} key={answer.answerID}>{answer.answer}</button>})}
             </div>
           </div>
           ) :  <div className="center-waiting"><p className="normal">Waiting for question...</p><div className="center"><img src="styles/ripple.gif" className="loading"/></div></div>
