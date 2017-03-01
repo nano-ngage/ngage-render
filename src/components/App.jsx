d9f97d83880cdd401173faab7cc086c7914e523a
Jordan Medina <jordanwmedina@gmail.com>
2017-03-01 10:13:33 -0800

    (feat) Add Modal container to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
+import Modal from '../containers/Modal.jsx';
+
 export default (props) => {
   return (
     <div classname="center">
       <div>{props.children}</div>
+      <Modal />
     </div>
     )
 }
