1c00f39d3ecc528618071591952b8b285fa95af7
pshen <patrick.lc.shen@gmail.com>
2017-03-03 15:18:22 -0800

    (feat) add onclick event

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,15 +12,18 @@ function roomVal(instance, e) {
   instance.setState({room: roomCode});
 }
 
-function handleClick(instance) {
-  instance.props.setSession({socket: instance.state.room, userID: instance.props.user ? instance.props.user.userID : -1})
-}
+// function handleClick(instance) {
+//   instance.props.setSession({socket: instance.state.room, userID: instance.props.user ? instance.props.user.userID : -1})
+// }
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      auth: new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN, props)
+      auth: new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN, props),
+      handleClick: () => {
+        this.props.setSession({socket: this.state.room, userID: this.props.user ? this.props.user.userID : -1})
+      }
     }
   }
 
@@ -42,11 +45,11 @@ class Home extends Component {
         <div className='room'>
           <input className='codeInput' value={this.state.room} onInput={linkEvent(this, roomVal)} type="text" placeholder="Code"
             onKeyPress={(e) => {if (e.which === 13) {this.props.setSession({socket: this.state.room, userID: this.props.user ? this.props.user.userID : -1})};}}></input>
-          <button className='button' onClick={linkEvent(this, handleClick)}>Enter</button>
+          <button className='button' onClick={this.state.handleClick.bind(this)} ontouchtap={()=>{alert('hihi')}} >Enter</button>
         </div>
         <div>
           {this.props.user ? (<div className='login'><button onClick={this.state.auth.logout.bind(this)}  className='button'>Logout</button></div>)
-          : (<div className='login'><button onClick={this.state.auth.login.bind(this)}  className='button'>Login</button></div>)}
+          : (<div className='login'><button onClick={this.state.auth.login.bind(this)} className='button'>Login</button></div>)}
         </div>
       </div>
     )
