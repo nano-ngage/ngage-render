fece09ed0b74d4471cd0bbbf188921baf1765a4b
Jordan Medina <jordanwmedina@gmail.com>
2017-03-03 14:41:56 -0800

    (refactor) Remove Modal container from App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
-import Modal from '../containers/Modal.jsx';
-
 export default (props) => {
   return (
     <div classname="center">
-      <div>{props.children}</div>
-      <Modal />
+      {props.children}
     </div>
     )
 }
