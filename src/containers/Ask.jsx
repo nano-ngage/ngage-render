8b0082477a000d5fba4029cb6fe15e338c1ac02c
Jordan Medina <jordanwmedina@gmail.com>
2017-03-06 18:42:47 -0800

    (feat) Update Ask to vary between presenter & viewer

--- src/containers/Ask.jsx
+++ src/containers/Ask.jsx
@@ -2,6 +2,7 @@ import Inferno from 'inferno';
 import Component from 'inferno-component';
 
 import { submitAudQuestion } from '../actions/audquestions';
+import { setQAModal } from '../actions/qa';
 import { connect } from 'inferno-redux';
 import { bindActionCreators } from 'redux';
 
@@ -15,6 +16,7 @@ class Ask extends Component {
 
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleQAModal = this.handleQAModal.bind(this);
   }
 
   handleInput(e) {
@@ -32,31 +34,56 @@ class Ask extends Component {
     this.props.submitAudQuestion(newAudQ)
   }
 
+  handleQAModal(e) {
+    e.preventDefault();
+    var toggle = e.target.value;
+    if (toggle === enable) {
+      this.props.setQAModal(true);
+    } else {
+      this.props.setQAModal(false);
+    }
+  }
+
   render() {
-    return (
-      <form className="modal-form" onSubmit={this.handleSubmit}>
-        <h3>Have a question for the presenter?</h3>
-        <div className="modal-form-group">
-          <input type="text"
-            placeholder="Ask a question..."
-            value={this.state.value}
-            onInput={this.handleInput} />
-            <button type="submit">Submit</button>
+    let ask;
+
+    if (!this.props.user || this.props.user.type === 0) {
+      ask = (
+        <form className="modal-form" onSubmit={this.handleSubmit}>
+          <h3>Have a question for the presenter?</h3>
+          <div className="modal-form-group">
+            <input type="text"
+              placeholder="Ask a question..."
+              value={this.state.value}
+              onInput={this.handleInput} />
+              <button type="submit">Submit</button>
+          </div>
+        </form>
+      )
+    } else if (this.props.user.type === 1) {
+      ask = (
+        <div className="modal-qa">
+          <h3>Allow participants to ask questions?</h3>
+          <button value="enable" type="text" onClick={this.handleQAModal}>Enable</button>
+          <button value="disable" type="text" onClick={this.handleQAModal}>Disable</button>
         </div>
-      </form>
-    );
+      )
+    }
+
+    return ask;
   }
 }
 
 const mapStateToProps = state => {
   return {
     user: state.user,
-    session: state.session
+    session: state.session,
+    qaModal: state.qaModal
   };
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators( { submitAudQuestion }, dispatch);
+  return bindActionCreators( { submitAudQuestion, setQAModal }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Ask);
