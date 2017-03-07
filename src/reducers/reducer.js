184cd3807b41b35132838bf00e76cf04ea1ac8c6
Jordan Medina <jordanwmedina@gmail.com>
2017-03-06 18:45:01 -0800

    (refactor) Update import/export for response reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 import type from './type';
 import user from './user';
 import question from './question';
+import response from './response';
 import { setAnswers, submitAnswer, showAnswer } from './answer';
 import { questions, askedQuestions } from './questions';
 import { audQuestions, submitAudQuestion, upvoteAudQuestion } from './audquestions';
-import { response } from './response';
 import { session, invalidRoom, presentation, pressession } from './session';
 
 export default combineReducers({
--- src/reducers/response.js
+++ src/reducers/response.js
@@ -1,6 +1,6 @@
 import { SETRESPONSES } from '../actions/response';
 
-export function response(state = null, action) {
+export default function response(state = null, action) {
   switch (action.type) {
     case SETRESPONSES:
       return state = action.response;
