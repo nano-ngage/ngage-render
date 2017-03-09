85ab18015e7db3ef6ebfe3f7acae53d224b868fd
Jordan Medina <jordanwmedina@gmail.com>
2017-03-09 11:34:28 -0800

    (feat) Add css imports to app entry point

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Inferno from 'inferno';
 import createRouter from './components/Router'
+import css from './css/main.css';
+import css2 from './css/animate.css';
+import css3 from './css/modal.css';
 
 Inferno.render(createRouter(), document.getElementById('app'));
