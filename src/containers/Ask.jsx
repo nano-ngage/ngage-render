7439bbab7d5e99af573a3adba5fd14c40643394e
Jordan Medina <jordanwmedina@gmail.com>
2017-03-08 19:21:04 -0800

    (feat) Add conditional rendering for Ask (viewer vs. presenter)

--- src/containers/Ask.jsx
+++ src/containers/Ask.jsx
@@ -2,7 +2,7 @@ import Inferno from 'inferno';
 import Component from 'inferno-component';
 
 import { submitAudQuestion } from '../actions/audquestions';
-import { setQAModal } from '../actions/qa';
+import { enableAsk, enableAudQ } from '../actions/qa';
 import { connect } from 'inferno-redux';
 import { bindActionCreators } from 'redux';
 
@@ -31,23 +31,30 @@ class Ask extends Component {
     const newAudQ = {
       content: this.state.value
     }
+    this.setState({
+      value: ''
+    });
+    this.props.handleToggle()
     this.props.submitAudQuestion(newAudQ)
   }
 
   handleQAModal(e) {
     e.preventDefault();
     var toggle = e.target.value;
-    if (toggle === enable) {
-      this.props.setQAModal(true);
-    } else {
-      this.props.setQAModal(false);
+    if (toggle === 'enableAsk') {
+      this.props.enableAsk(1);
+    } else if (toggle === 'disableAsk') {
+      this.props.enableAsk(0);
+    } else if (toggle === 'enableAudQ') {
+      this.props.enableAudQ(1);
+    } else if (toggle === 'disableAudQ') {
+      this.props.enableAudQ(0);
     }
   }
 
   render() {
     let ask;
-
-    if (!this.props.user || this.props.user.type === 0) {
+    if (!this.props.user || this.props.user.type === 1) {
       ask = (
         <form className="modal-form" onSubmit={this.handleSubmit}>
           <h3>Have a question for the presenter?</h3>
@@ -60,12 +67,29 @@ class Ask extends Component {
           </div>
         </form>
       )
-    } else if (this.props.user.type === 1) {
+    } else if (this.props.user.type === 0) {
       ask = (
-        <div className="modal-qa">
-          <h3>Allow participants to ask questions?</h3>
-          <button value="enable" type="text" onClick={this.handleQAModal}>Enable</button>
-          <button value="disable" type="text" onClick={this.handleQAModal}>Disable</button>
+        <div className="modal-enable">
+          <div className="modal-enable-ask">
+            <h3>Allow participants to ask questions?</h3>
+            <div className="modal-enable-buttons">
+              <button value={this.props.askEnabled ? 'disableAsk' : 'enableAsk'}
+                      type="text"
+                      onClick={this.handleQAModal}>
+                {this.props.askEnabled ? 'Disable Asking Questions' : 'Enable Asking Questions'}
+              </button>
+            </div>
+          </div>
+          <div className="modal-enable-audq">
+            <h3>Allow participants to see other audience questions?</h3>
+            <div className="modal-enable-buttons">
+              <button value={this.props.audQEnabled ? 'disableAudQ' : 'enableAudQ'}
+                      type="text"
+                      onClick={this.handleQAModal}>
+                {this.props.audQEnabled ? 'Hide Audience Questions' : 'Show Audience Questions'}
+              </button>
+            </div>
+          </div>
         </div>
       )
     }
@@ -78,12 +102,13 @@ const mapStateToProps = state => {
   return {
     user: state.user,
     session: state.session,
-    qaModal: state.qaModal
+    askEnabled: state.askEnabled,
+    audQEnabled: state.audQEnabled
   };
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators( { submitAudQuestion, setQAModal }, dispatch);
+  return bindActionCreators( { submitAudQuestion, enableAsk, enableAudQ }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Ask);
