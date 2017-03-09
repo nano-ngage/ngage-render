0ba653f4d3d6d3fa6459ed9e04fd86cfebb9cbb5
Jordan Medina <jordanwmedina@gmail.com>
2017-03-08 19:27:15 -0800

    (feat) Split modal enable between Ask and audQ

--- src/actions/qa.js
+++ src/actions/qa.js
@@ -1,8 +1,16 @@
-export const SETQAMODAL = 'SETQAMODAL';
+export const ENABLEASK = 'ENABLEASK';
+export const ENABLEAUDQ = 'ENABLEAUDQ';
 
-export function setQAModal(qaModal) {
+export function enableAsk(askEnabled) {
   return {
-    type: SETQAMODAL,
-    qaModal: qaModal
+    type: ENABLEASK,
+    askEnabled: askEnabled
+  };
+}
+
+export function enableAudQ(audQEnabled) {
+  return {
+    type: ENABLEAUDQ,
+    audQEnabled: audQEnabled
   };
 }
