45ade9fc456eb1f659e2543ccf6bb43fde1cd9ab
Jordan Medina <jordanwmedina@gmail.com>
2017-03-09 18:01:28 -0800

    (feat) Add audQuestionUpvoted action

--- src/actions/audquestions.js
+++ src/actions/audquestions.js
@@ -1,6 +1,7 @@
 export const SUBMITAUDQUESTION = 'SUBMITAUDQUESTION';
 export const SETAUDQUESTIONS = 'SETAUDQUESTIONS';
 export const UPVOTEAUDQUESTION = 'UPVOTEAUDQUESTION';
+export const AUDQUESTIONUPVOTED = 'AUDQUESTIONUPVOTED';
 
 export function setAudQuestions(audQuestions) {
   return {
@@ -20,5 +21,12 @@ export function upvoteAudQuestion(audQuestion) {
   return {
     type: UPVOTEAUDQUESTION,
     audQuestion: audQuestion
+  };
+}
+
+export function audQuestionUpvoted(audQuestionID) {
+  return {
+    type: AUDQUESTIONUPVOTED,
+    audQuestionID: audQuestionID
   };
 }
